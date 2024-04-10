005fb1a29d9b46dccf25bb847a73041d56849df9
JohnVonNeumann <louiswillcock@gmail.com>
2024-04-10 13:28:26 +1000

    UPDATE product_footprint.py - add spec_version attr to ProductFootprint class

--- pathfinder_framework/product_footprint/product_footprint.py
+++ pathfinder_framework/product_footprint/product_footprint.py
@@ -15,8 +15,17 @@ class ProductFootprint:
         Args:
             id (ProductFootprintId | None): The ProductFootprintId for this instance. If not provided, a new one
                 will be generated.
+
+        Attributes:
+            id (ProductFootprintId): The unique identifier for this ProductFootprint.
+            spec_version (str): The version of the ProductFootprint data specification. Currently, hardcoded to "2.0.0",
+                but may be updated in the future to support different versions. There is a little confusion between
+                the definition in the technical documentation and the framework documentation, and the steering council
+                will be looking to implement this in a more SemVer friendly way, which it currently is not. So just
+                introduce it hard-coded for now, and revise it later.
         """
         self.id = id if id else ProductFootprintId()
+        self.spec_version = "2.0.0"
 
     def __repr__(self) -> str:
         """Returns a string representation of the ProductFootprint instance."""
--- tests/product_footprint/test_product_footprint.py
+++ tests/product_footprint/test_product_footprint.py
@@ -15,3 +15,17 @@ def test_product_footprint_id_default_initialization():
     """Tests that a ProductFootprint instance initializes a ProductFootprintId by default."""
     product_footprint = ProductFootprint()
     assert isinstance(product_footprint.id, ProductFootprintId)
+
+
+def test_product_footprint_spec_version():
+    """Tests that a ProductFootprint instance has a specVersion attribute."""
+    product_footprint = ProductFootprint()
+    assert product_footprint.spec_version == "2.0.0"
+
+
+def test_product_footprint_repr():
+    """Tests that the __repr__ method returns the expected string representation."""
+    product_footprint_id = ProductFootprintId()
+    product_footprint = ProductFootprint(id=product_footprint_id)
+    expected_repr = f"ProductFootprint(id={product_footprint_id})"
+    assert repr(product_footprint) == expected_repr
