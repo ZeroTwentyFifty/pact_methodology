76acda03f9182904d7a8bb6b5f32feb6c8b2fb81
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-03 23:09:53 +1000

    CREATE cross_sectoral_standard.py - introduce CrossSectoralStandard enum

---
+++ pathfinder_framework/carbon_footprint/cross_sectoral_standard.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class CrossSectoralStandard(str, Enum):
+    """
+    CrossSectoralStandard is the enumeration of accounting standards used for product carbon footprint calculation. Valid values are
+
+    - GHG Protocol Product standard: for the GHG Protocol Product standard
+    - ISO Standard 14067: for ISO Standard 14067
+    - ISO Standard 14044: for ISO Standard 14044
+
+    4.9.1. JSON Representation
+    Each CrossSectoralStandard MUST be encoded as a JSON string.
+
+    The value of each DeclaredUnit MUST be encoded as a JSON String.
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#dt-crosssectoralstandard
+    """
+    GHG_PROTOCOL = 'GHG Protocol Product standard'
+    ISO_14067 = 'ISO Standard 14067'
+    ISO_14044 = 'ISO Standard 14044'
---
+++ tests/carbon_footprint/test_cross_sectoral_standard.py
@@ -0,0 +1,7 @@
+from pathfinder_framework.carbon_footprint.cross_sectoral_standard import CrossSectoralStandard
+
+
+def test_cross_sectoral_standard_values():
+    assert CrossSectoralStandard.GHG_PROTOCOL == 'GHG Protocol Product standard'
+    assert CrossSectoralStandard.ISO_14067 == 'ISO Standard 14067'
+    assert CrossSectoralStandard.ISO_14044 == 'ISO Standard 14044'
