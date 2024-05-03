2f565447259d2c4936e4b53397fe3df5d75b837b
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-03 23:16:39 +1000

    CREATE declared_unit.py - introduce DeclaredUnit enum

---
+++ pathfinder_framework/carbon_footprint/declared_unit.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class DeclaredUnit(str, Enum):
+    """
+    DeclaredUnit is the enumeration of accepted declared units with values.
+
+    The values are based on the International System of Units (SI) as defined by the International Committee for Weights and Measures (ICWM).
+
+    - LITER: for special SI Unit litre
+    - KILOGRAM: for SI Base Unit kilogram
+    - CUBIC_METER: for cubic meter, the Derived Unit from SI Base Unit metre
+    - KILOWATT_HOUR: for kilowatt-hour, the Derived Unit from special SI Unit watt
+    - MEGAJOULE: for megajoule, the Derived Unit from special SI Unit joule
+    - TON_KILOMETER: for ton kilometer, the Derived Unit from SI Base Units kilogram and metre
+    - SQUARE_METER: for square meter, the Derived Unit from SI Base Unit metre
+
+    The value of each DeclaredUnit MUST be encoded as a JSON String.
+
+    For more information, please refer to the official documentation:
+        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#dt-declaredunit
+    And the SI Unit reference:
+        https://www.bipm.org/documents/20126/41483022/SI-Brochure-9-EN.pdf/2d2b50bf-f2b4-9661-f402-5f9d66e4b507
+    """
+    LITER = 'liter'
+    KILOGRAM = 'kilogram'
+    CUBIC_METER = 'cubic meter'
+    KILOWATT_HOUR = 'kilowatt hour'
+    MEGAJOULE = 'megajoule'
+    TON_KILOMETER = 'ton kilometer'
+    SQUARE_METER = 'square meter'
---
+++ tests/carbon_footprint/test_declared_unit.py
@@ -0,0 +1,11 @@
+from pathfinder_framework.carbon_footprint.declared_unit import DeclaredUnit
+
+
+def test_declared_unit_values():
+    assert DeclaredUnit.LITER == 'liter'
+    assert DeclaredUnit.KILOGRAM == 'kilogram'
+    assert DeclaredUnit.CUBIC_METER == 'cubic meter'
+    assert DeclaredUnit.KILOWATT_HOUR == 'kilowatt hour'
+    assert DeclaredUnit.MEGAJOULE == 'megajoule'
+    assert DeclaredUnit.TON_KILOMETER == 'ton kilometer'
+    assert DeclaredUnit.SQUARE_METER == 'square meter'
