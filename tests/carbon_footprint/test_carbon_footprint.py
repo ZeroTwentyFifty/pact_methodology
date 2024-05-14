3ae8e11c041d9abb1c14c21b34a44f20293bbfee
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-14 12:28:23 +1000

    UPDATE carbon_footprint.py - refactor datatype for declared_unit attribute

--- tests/carbon_footprint/test_carbon_footprint.py
+++ tests/carbon_footprint/test_carbon_footprint.py
@@ -2,6 +2,7 @@ import pytest
 
 from pathfinder_framework.carbon_footprint.carbon_footprint import CarbonFootprint
 from pathfinder_framework.carbon_footprint.characterization_factors import CharacterizationFactors
+from pathfinder_framework.carbon_footprint.declared_unit import DeclaredUnit
 
 
 def test_carbon_footprint_exists():
@@ -9,13 +10,13 @@ def test_carbon_footprint_exists():
 
 
 def test_carbon_footprint_instantiation():
-    carbon_footprint = CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+    carbon_footprint = CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
     assert isinstance(carbon_footprint, CarbonFootprint)
 
 
 def test_carbon_footprint_attributes():
-    carbon_footprint = CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
-    assert carbon_footprint.declared_unit == "kilogram"
+    carbon_footprint = CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+    assert carbon_footprint.declared_unit == DeclaredUnit.KILOGRAM
     assert carbon_footprint.unitary_product_amount == 1.0
     assert carbon_footprint.p_cf_excluding_biogenic == 0.5
     assert carbon_footprint.fossil_ghg_emissions == 0.3
@@ -29,55 +30,57 @@ def test_carbon_footprint_attributes():
 
 
 def test_carbon_footprint_invalid_declared_unit():
-    with pytest.raises(ValueError):
-        CarbonFootprint("", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+    with pytest.raises(ValueError) as excinfo:
+        CarbonFootprint("invalid unit", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+    assert str(excinfo.value) == "declared_unit 'invalid unit' is not valid. It must be one of the following: liter, kilogram, cubic meter, kilowatt hour, megajoule, ton kilometer, square meter"
 
 
 def test_carbon_footprint_invalid_unitary_product_amount():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 0.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 0.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_p_cf_excluding_biogenic():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, -0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, -0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_fossil_ghg_emissions():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, -0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, -0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_fossil_carbon_content():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, -0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, -0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_biogenic_carbon_content():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, -0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, -0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_characterization_factors():
-    with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, "AR7", ["AR6"], ["Standard"], "boundary processes description", 1.0)
+    with pytest.raises(ValueError) as excinfo:
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, "AR7", ["AR6"], ["Standard"], "boundary processes description", 1.0)
+    assert str(excinfo.value) == "characterization_factors must be an instance of CharacterizationFactors"
 
 
 def test_carbon_footprint_invalid_ipcc_characterization_factors_sources():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, [], ["Standard"], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, [], ["Standard"], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_cross_sectoral_standards_used():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], [], "boundary processes description", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], [], "boundary processes description", 1.0)
 
 
 def test_carbon_footprint_invalid_boundary_processes_description():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "", 1.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "", 1.0)
 
 
 def test_carbon_footprint_invalid_exempted_emissions_percent():
     with pytest.raises(ValueError):
-        CarbonFootprint("kilogram", 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 6.0)
+        CarbonFootprint(DeclaredUnit.KILOGRAM, 1.0, 0.5, 0.3, 0.2, 0.1, CharacterizationFactors.AR6, ["AR6"], ["Standard"], "boundary processes description", 6.0)
