9fb931ea3aa68304e753b044369a8cced2948ed9
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-18 10:36:50 +1000

    UPDATE test_reference_period.py - add test and tidy file

--- tests/carbon_footprint/test_reference_period.py
+++ tests/carbon_footprint/test_reference_period.py
@@ -1,6 +1,8 @@
 import pytest
+
 from pathfinder_framework.carbon_footprint.reference_period import ReferencePeriod
 from pathfinder_framework.datetime import DateTime
+
 
 def test_reference_period_init():
     start = DateTime("2022-01-01T00:00:00Z")
@@ -9,14 +11,23 @@ def test_reference_period_init():
     assert ref_period.start == start
     assert ref_period.end == end
 
+
 def test_reference_period_init_invalid_start():
     with pytest.raises(ValueError):
-        start = "2022-01-01T00:00:00"  # missing 'Z'
+        start = "2022-01-01T00:00:00"
         end = DateTime("2022-12-31T23:59:59Z")
         ReferencePeriod(start, end)
+
 
 def test_reference_period_init_invalid_end():
     with pytest.raises(ValueError):
         start = DateTime("2022-01-01T00:00:00Z")
-        end = "2022-12-31T23:59:59"  # missing 'Z'
+        end = "2022-12-31T23:59:59"
+        ReferencePeriod(start, end)
+
+
+def test_reference_period_end_before_start():
+    with pytest.raises(ValueError, match="Start date must be before end date"):
+        start = DateTime("2022-01-01T00:00:00Z")
+        end = DateTime("2021-01-01T00:00:00Z")
         ReferencePeriod(start, end)
