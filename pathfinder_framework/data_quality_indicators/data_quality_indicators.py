991597fc99ea385fd91e9fbb795a129c6886f519
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-24 09:46:47 +1000

    UPDATE data_quality_indictors.py - type annotate the DQR class as arg type for the DQI init

--- pathfinder_framework/data_quality_indicators/data_quality_indicators.py
+++ pathfinder_framework/data_quality_indicators/data_quality_indicators.py
@@ -1,38 +1,37 @@
-from __future__ import annotations  # For forward references within the module
-
-
 from pathfinder_framework.carbon_footprint.reference_period import ReferencePeriod
+from pathfinder_framework.data_quality_indicators.data_quality_rating import DataQualityRating
 
 
 class DataQualityIndicators:
 
     def __init__(self, reference_period: ReferencePeriod, coverage_percent: float | None = None,
-                 technological_dqr: float | None = None, temporal_dqr: float | None = None,
-                 geographical_dqr: float | None = None, completeness_dqr: float | None = None,
-                 reliability_dqr: float | None = None):
+                 technological_dqr: DataQualityRating | None = None, temporal_dqr: DataQualityRating | None = None,
+                 geographical_dqr: DataQualityRating | None = None, completeness_dqr: DataQualityRating | None = None,
+                 reliability_dqr: DataQualityRating | None = None):
         """Represents quantitative data quality indicators.
 
         Attributes:
-            coverage_percent: Percentage of PCF included in the assessment (>5% emissions threshold).
-            technological_dqr: Data quality rating for technological representativeness (1-3).
-            temporal_dqr: Data quality rating for temporal representativeness (1-3).
-            geographical_dqr: Data quality rating for geographical representativeness (1-3).
-            completeness_dqr: Data quality rating for data completeness (1-3).
-            reliability_dqr: Data quality rating for data reliability (1-3).
+            coverage_percent (float): Percentage of PCF included in the assessment (>5% emissions threshold).
+            technological_dqr (DataQualityRating): Data quality rating for technological representativeness.
+            temporal_dqr (DataQualityRating): Data quality rating for temporal representativeness.
+            geographical_dqr (DataQualityRating): Data quality rating for geographical representativeness.
+            completeness_dqr (DataQualityRating): Data quality rating for data completeness.
+            reliability_dqr (DataQualityRating): Data quality rating for data reliability.
 
         Args:
             reference_period (ReferencePeriod): The reference period for the data quality assessment.
-            coverage_percent (float, optional): defaults to None if before 2025.
-            technological_dqr (float, optional): defaults to None if before 2025.
-            temporal_dqr (float, optional): defaults to None if before 2025.
-            geographical_dqr (float, optional): defaults to None if before 2025.
-            completeness_dqr (float, optional): defaults to None if before 2025.
-            reliability_dqr (float, optional): defaults to None if before 2025.
+            coverage_percent (float, optional): Defaults to None if before 2025.
+            technological_dqr (DataQualityRating, optional): Defaults to None if before 2025.
+            temporal_dqr (DataQualityRating, optional): Defaults to None if before 2025.
+            geographical_dqr (DataQualityRating, optional): Defaults to None if before 2025.
+            completeness_dqr (DataQualityRating, optional): Defaults to None if before 2025.
+            reliability_dqr (DataQualityRating, optional): Defaults to None if before 2025.
 
         Raises:
-            ValueError: If any DQR value is not between 1 and 3, or if a required attribute
-                        is missing or None for reporting periods including 2025 or later.
+            ValueError: If a required attribute is missing or None for reporting periods
+                        including 2025 or later.
         """
+
         self.coverage_percent = coverage_percent
         self.technological_dqr = technological_dqr
         self.temporal_dqr = temporal_dqr
@@ -47,9 +46,5 @@ class DataQualityIndicators:
             for attr in required_attributes_after_2025:
                 if not hasattr(self, attr) or getattr(self, attr) is None:
                     raise ValueError(
-                        f"Attribute '{attr}' must be defined and not None for reporting periods including 2025 or later")
-
-        # Validate DQR values (should be between 1 and 3)
-        for attr in required_attributes_after_2025[1:]:
-            if getattr(self, attr) is not None and not 1 <= getattr(self, attr) <= 3:
-                raise ValueError(f"Attribute '{attr}' must be between 1 and 3")
+                        f"Attribute '{attr}' must be defined and not None for reporting periods including 2025 or later"
+                    )
