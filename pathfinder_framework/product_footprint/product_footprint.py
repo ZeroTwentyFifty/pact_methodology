2137792b3b3a04140009cc4e7edd9af6305c7b27
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-27 12:21:01 +1000

    UPDATE product_footprint.py - add pcf attribute to ProductFootprint

--- pathfinder_framework/product_footprint/product_footprint.py
+++ pathfinder_framework/product_footprint/product_footprint.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from pathfinder_framework.carbon_footprint.carbon_footprint import CarbonFootprint
 from pathfinder_framework.product_footprint.id import ProductFootprintId
 from pathfinder_framework.product_footprint.status import ProductFootprintStatus
 from pathfinder_framework.urn import CompanyId, ProductId
@@ -12,19 +13,35 @@ class ProductFootprint:
     """
     Represents the carbon footprint of a product under a specific scope and with values calculated in accordance
     with the Pathfinder Framework.
+
+    Attributes:
+        id (ProductFootprintId): The unique identifier for this ProductFootprint.
+        spec_version (str): The version of the ProductFootprint data specification.
+        version (Version): The version of the ProductFootprint.
+        created (DateTime): The date and time when the ProductFootprint was created.
+        updated (DateTime): The date and time when the ProductFootprint was last updated.
+        status (ProductFootprintStatus): The status of the ProductFootprint.
+        status_comment (str): A comment describing the status of the ProductFootprint.
+        validity_period_start (DateTime): The start date of the validity period for the ProductFootprint.
+        validity_period_end (DateTime): The end date of the validity period for the ProductFootprint.
+        company_name (str): The name of the company that owns the ProductFootprint.
+        company_ids (list[CompanyId]): A list of CompanyIds for the company that owns the ProductFootprint.
+        product_description (str): A description of the product.
+        product_ids (list[ProductId]): A list of ProductIds for the product.
+        product_category_cpc (str): The category of the product according to the CPC (Central Product Classification) system.
+        product_name_company (str): The name of the product as used by the company.
+        comment (str): A comment about the ProductFootprint.
+        extensions (dict): A dictionary of additional metadata about the ProductFootprint.
+        pcf (CarbonFootprint): The carbon footprint of the given product with value conforming to the data type CarbonFootprint.
     """
 
-    def __init__(self, *, id: ProductFootprintId | None = None, spec_version: str = "2.0.0", version: Version, created: DateTime, updated: DateTime, status: ProductFootprintStatus, status_comment: str, validity_period_start: DateTime, validity_period_end: DateTime, company_name: str, company_ids: list[CompanyId], product_description: str, product_ids: list[ProductId], product_category_cpc: CPC, product_name_company: str, comment: str, extensions: dict):
-        """Initializes a new ProductFootprint instance.
+    def __init__(self, *, id: ProductFootprintId | None = None, spec_version: str = "2.0.0", version: Version, created: DateTime, updated: DateTime, status: ProductFootprintStatus, status_comment: str, validity_period_start: DateTime, validity_period_end: DateTime, company_name: str, company_ids: list[CompanyId], product_description: str, product_ids: list[ProductId], product_category_cpc: CPC, product_name_company: str, comment: str, extensions: dict, pcf: CarbonFootprint):
+        """
+        Initializes a new ProductFootprint instance.
 
         Args:
-            id (ProductFootprintId | None): The ProductFootprintId for this instance. If not provided, a new one
-                will be generated.
-            spec_version (str): The version of the ProductFootprint data specification. Currently, hardcoded to "2.0.0",
-                but may be updated in the future to support different versions. There is a little confusion between
-                the definition in the technical documentation and the framework documentation, and the steering council
-                will be looking to implement this in a more SemVer friendly way, which it currently is not. So just
-                introduce it hard-coded for now, and revise it later.
+            id (ProductFootprintId | None): The ProductFootprintId for this instance. If not provided, a new one will be generated.
+            spec_version (str): The version of the ProductFootprint data specification. Currently, hardcoded to "2.0.0", but may be updated in the future to support different versions.
             version (Version): The version of the ProductFootprint.
             created (DateTime): The date and time when the ProductFootprint was created.
             updated (DateTime): The date and time when the ProductFootprint was last updated.
@@ -40,26 +57,12 @@ class ProductFootprint:
             product_name_company (str): The name of the product as used by the company.
             comment (str): A comment about the ProductFootprint.
             extensions (dict): A dictionary of additional metadata about the ProductFootprint.
-
-        Attributes:
-            id (ProductFootprintId): The unique identifier for this ProductFootprint.
-            spec_version (str): The version of the ProductFootprint data specification.
-            version (Version): The version of the ProductFootprint.
-            created (DateTime): The date and time when the ProductFootprint was created.
-            updated (DateTime): The date and time when the ProductFootprint was last updated.
-            status (ProductFootprintStatus): The status of the ProductFootprint.
-            status_comment (str): A comment describing the status of the ProductFootprint.
-            validity_period_start (DateTime): The start date of the validity period for the ProductFootprint.
-            validity_period_end (DateTime): The end date of the validity period for the ProductFootprint.
-            company_name (str): The name of the company that owns the ProductFootprint.
-            company_ids (list[CompanyId]): A list of CompanyIds for the company that owns the ProductFootprint.
-            product_description (str): A description of the product.
-            product_ids (list[ProductId]): A list of ProductIds for the product.
-            product_category_cpc (str): The category of the product according to the CPC (Central Product Classification) system.
-            product_name_company (str): The name of the product as used by the company.
-            comment (str): A comment about the ProductFootprint.
-            extensions (dict): A dictionary of additional metadata about the ProductFootprint.
+            pcf (CarbonFootprint): The carbon footprint of the given product with value conforming to the data type CarbonFootprint.
         """
+
+        if not isinstance(pcf, CarbonFootprint):
+            raise ValueError("pcf must be an instance of CarbonFootprint")
+
         self.id = id if id else ProductFootprintId()
         self.spec_version = spec_version
         self.version = version
@@ -77,6 +80,7 @@ class ProductFootprint:
         self.product_name_company = product_name_company
         self.comment = comment
         self.extensions = extensions
+        self.pcf = pcf
 
     def __repr__(self) -> str:
         """Returns a string representation of the ProductFootprint instance."""
--- tests/product_footprint/test_product_footprint.py
+++ tests/product_footprint/test_product_footprint.py
@@ -2,6 +2,7 @@ import pytest
 
 from datetime import datetime
 
+from pathfinder_framework.carbon_footprint.carbon_footprint import CarbonFootprint
 from pathfinder_framework.product_footprint.id import ProductFootprintId
 from pathfinder_framework.product_footprint.product_footprint import ProductFootprint
 from pathfinder_framework.product_footprint.status import ProductFootprintStatus
@@ -9,6 +10,16 @@ from pathfinder_framework.urn import CompanyId, ProductId
 from pathfinder_framework.product_footprint.cpc import CPCCodeLookup, CPC
 from pathfinder_framework.product_footprint.version import Version
 from pathfinder_framework.datetime import DateTime
+from pathfinder_framework.carbon_footprint.carbon_footprint import CarbonFootprint
+from pathfinder_framework.carbon_footprint.characterization_factors import CharacterizationFactors
+from pathfinder_framework.carbon_footprint.cross_sectoral_standard import CrossSectoralStandard
+from pathfinder_framework.carbon_footprint.declared_unit import DeclaredUnit
+from pathfinder_framework.datetime import DateTime
+from pathfinder_framework.carbon_footprint.reference_period import ReferencePeriod
+from pathfinder_framework.carbon_footprint.geographical_scope import (
+    CarbonFootprintGeographicalScope, GeographicalGranularity
+)
+from pathfinder_framework.data_quality_indicators.data_quality_indicators import DataQualityIndicators
 
 
 @pytest.fixture(scope="module")
@@ -31,8 +42,46 @@ def valid_cpc() -> CPC:
 def version() -> Version:
     return Version(1)
 
+@pytest.fixture
+def valid_carbon_footprint_data():
+    return {
+        "declared_unit": DeclaredUnit.KILOGRAM,
+        "unitary_product_amount": 1.0,
+        "p_cf_excluding_biogenic": 0.5,
+        "p_cf_including_biogenic": 2,
+        "fossil_ghg_emissions": 0.3,
+        "fossil_carbon_content": 0.2,
+        "biogenic_carbon_content": 0.1,
+        "characterization_factors": CharacterizationFactors.AR6,
+        "ipcc_characterization_factors_sources": ["AR6"],
+        "cross_sectoral_standards_used": [CrossSectoralStandard.GHG_PROTOCOL],
+        "boundary_processes_description": "boundary processes description",
+        "exempted_emissions_percent": 1.0,
+        "reference_period": ReferencePeriod(start=DateTime.now(), end=DateTime.now()),
+        "packaging_emissions_included": True,
+        "geographical_scope": CarbonFootprintGeographicalScope(global_scope=True, geography_country_subdivision=None,
+                                                               geography_country=None,
+                                                               geography_region_or_subregion=None),
+        "primary_data_share": 50.0,
+        "dqi": DataQualityIndicators(reference_period=ReferencePeriod(start=DateTime.now(), end=DateTime.now())),
+        "d_luc_ghg_emissions": 2,
+        "land_management_ghg_emissions": 1.0,
+        "other_biogenic_ghg_emissions": 1.5,
+        "biogenic_carbon_withdrawal": -1.0,
+        "iluc_ghg_emissions": 1.0,
+        "aircraft_ghg_emissions": 1.0,
+        "packaging_ghg_emissions": 1.0,
+        "allocation_rules_description": "Example allocation rules description",
+        "uncertainty_assessment_description": "Example uncertainty assessment description"
+    }
 
-def test_product_footprint_initialization(company_ids, product_ids, valid_cpc, version):
+
+@pytest.fixture
+def carbon_footprint(valid_carbon_footprint_data):
+    return CarbonFootprint(**valid_carbon_footprint_data)
+
+
+def test_product_footprint_initialization(company_ids, product_ids, valid_cpc, version, carbon_footprint):
     product_footprint_id = ProductFootprintId()
     product_footprint = ProductFootprint(
         id=product_footprint_id,
@@ -50,7 +99,8 @@ def test_product_footprint_initialization(company_ids, product_ids, valid_cpc, v
         product_category_cpc=valid_cpc,
         product_name_company="Product Name Company",
         comment="This is a comment",
-        extensions={"key": "value"}
+        extensions={"key": "value"},
+        pcf=carbon_footprint
     )
     assert product_footprint.id == product_footprint_id
     assert product_footprint.version == version
@@ -68,9 +118,11 @@ def test_product_footprint_initialization(company_ids, product_ids, valid_cpc, v
     assert product_footprint.product_name_company == "Product Name Company"
     assert product_footprint.comment == "This is a comment"
     assert product_footprint.extensions == {"key": "value"}
+    assert isinstance(product_footprint.pcf, CarbonFootprint)
 
 
-def test_product_footprint_default_initialization(company_ids, product_ids, valid_cpc, version):
+
+def test_product_footprint_default_initialization(company_ids, product_ids, valid_cpc, version, carbon_footprint):
     """Tests that a ProductFootprint instance initializes with default values for optional attributes."""
     product_footprint = ProductFootprint(
         version=version,
@@ -87,12 +139,13 @@ def test_product_footprint_default_initialization(company_ids, product_ids, vali
         product_category_cpc=valid_cpc,
         product_name_company="Product Name Company",
         comment="This is a comment",
-        extensions={"key": "value"}
+        extensions={"key": "value"},
+        pcf=carbon_footprint
     )
     assert isinstance(product_footprint.id, ProductFootprintId)
 
 
-def test_product_footprint_spec_version(company_ids, product_ids, valid_cpc, version):
+def test_product_footprint_spec_version(company_ids, product_ids, valid_cpc, version, carbon_footprint):
     """Tests that a ProductFootprint instance has a specVersion attribute."""
     product_footprint = ProductFootprint(
         version=version,
@@ -109,12 +162,13 @@ def test_product_footprint_spec_version(company_ids, product_ids, valid_cpc, ver
         product_category_cpc=valid_cpc,
         product_name_company="Product Name Company",
         comment="This is a comment",
-        extensions={"key": "value"}
+        extensions={"key": "value"},
+        pcf=carbon_footprint
     )
     assert product_footprint.spec_version == "2.0.0"
 
 
-def test_product_footprint_repr(company_ids, product_ids, valid_cpc, version):
+def test_product_footprint_repr(company_ids, product_ids, valid_cpc, version, carbon_footprint):
     """Tests that the __repr__ method returns the expected string representation."""
     product_footprint_id = ProductFootprintId()
     product_footprint = ProductFootprint(
@@ -133,7 +187,50 @@ def test_product_footprint_repr(company_ids, product_ids, valid_cpc, version):
         product_category_cpc=valid_cpc,
         product_name_company="Product Name Company",
         comment="This is a comment",
-        extensions={"key": "value"}
+        extensions={"key": "value"},
+        pcf=carbon_footprint
     )
     expected_repr = f"ProductFootprint(id={product_footprint_id})"
     assert repr(product_footprint) == expected_repr
+
+def test_product_footprint_pcf_validation(company_ids, product_ids, valid_cpc, version, carbon_footprint):
+    with pytest.raises(ValueError):
+        ProductFootprint(
+            version=version,
+            created=DateTime.now(),
+            updated=DateTime.now(),
+            status=ProductFootprintStatus.ACTIVE,
+            status_comment="This is a comment",
+            validity_period_start=DateTime.now(),
+            validity_period_end=DateTime.now(),
+            company_name="Company Name",
+            company_ids=company_ids,
+            product_description="Product Description",
+            product_ids=product_ids,
+            product_category_cpc=valid_cpc,
+            product_name_company="Product Name Company",
+            comment="This is a comment",
+            extensions={"key": "value"},
+            pcf="Invalid pcf"
+        )
+
+def test_product_footprint_pcf_validation_success(company_ids, product_ids, valid_cpc, version, carbon_footprint):
+    product_footprint = ProductFootprint(
+        version=version,
+        created=DateTime.now(),
+        updated=DateTime.now(),
+        status=ProductFootprintStatus.ACTIVE,
+        status_comment="This is a comment",
+        validity_period_start=DateTime.now(),
+        validity_period_end=DateTime.now(),
+        company_name="Company Name",
+        company_ids=company_ids,
+        product_description="Product Description",
+        product_ids=product_ids,
+        product_category_cpc=valid_cpc,
+        product_name_company="Product Name Company",
+        comment="This is a comment",
+        extensions={"key": "value"},
+        pcf=carbon_footprint
+    )
+    assert isinstance(product_footprint.pcf, CarbonFootprint)
