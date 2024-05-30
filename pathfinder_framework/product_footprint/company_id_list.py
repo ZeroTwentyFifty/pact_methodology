4cc2d06cc6202bbdbe2e120fb4b5ed07f470c94c
JohnVonNeumann <louiswillcock@gmail.com>
2024-05-30 10:17:15 +1000

    UPDATE {company,product}_id_list.py - add docs and complete implementations

--- pathfinder_framework/product_footprint/company_id_list.py
+++ pathfinder_framework/product_footprint/company_id_list.py
@@ -2,7 +2,28 @@ from pathfinder_framework.urn import CompanyId
 
 
 class CompanyIdList:
+    """
+    A list of CompanyId objects.
+
+    Attributes:
+        company_ids (list): A list of CompanyId objects.
+
+    Raises:
+        ValueError: If company_ids is not a list of CompanyId objects.
+        ValueError: If there are duplicate CompanyId objects in the list.
+    """
+
     def __init__(self, company_ids):
+        """
+        Initializes a CompanyIdList object.
+
+        Args:
+            company_ids (list): A list of CompanyId objects.
+
+        Raises:
+            ValueError: If company_ids is not a list of CompanyId objects.
+            ValueError: If there are duplicate CompanyId objects in the list.
+        """
         if not isinstance(company_ids, list) or not all(isinstance(company_id, CompanyId) for company_id in company_ids):
             raise ValueError("company_ids must be a list of CompanyId")
         if len(set(company_ids)) != len(company_ids):
@@ -10,10 +31,104 @@ class CompanyIdList:
         self.company_ids = company_ids
 
     def __iter__(self):
+        """
+        Returns an iterator over the CompanyId objects in the list.
+
+        Returns:
+            iterator: An iterator over the CompanyId objects in the list.
+        """
         return iter(self.company_ids)
 
     def __len__(self):
+        """
+        Returns the number of CompanyId objects in the list.
+
+        Returns:
+            int: The number of CompanyId objects in the list.
+        """
         return len(self.company_ids)
 
     def __getitem__(self, index):
+        """
+        Returns the CompanyId object at the specified index.
+
+        Args:
+            index (int): The index of the CompanyId object to return.
+
+        Returns:
+            CompanyId: The CompanyId object at the specified index.
+        """
         return self.company_ids[index]
+
+    def __setitem__(self, index, value):
+        """
+        Sets the CompanyId object at the specified index.
+
+        Args:
+            index (int): The index of the CompanyId object to set.
+            value (CompanyId): The CompanyId object to set.
+
+        Raises:
+            ValueError: If value is not a CompanyId object.
+        """
+        if not isinstance(value, CompanyId):
+            raise ValueError("company_id must be an instance of CompanyId")
+        self.company_ids[index] = value
+
+    def __delitem__(self, index):
+        """
+        Deletes the CompanyId object at the specified index.
+
+        Args:
+            index (int): The index of the CompanyId object to delete.
+        """
+        del self.company_ids[index]
+
+    def append(self, company_id):
+        """
+        Appends a CompanyId object to the list.
+
+        Args:
+            company_id (CompanyId): The CompanyId object to append.
+
+        Raises:
+            ValueError: If company_id is not a CompanyId object.
+            ValueError: If company_id is already in the list.
+        """
+        if not isinstance(company_id, CompanyId):
+            raise ValueError("company_id must be an instance of CompanyId")
+        if company_id in self.company_ids:
+            raise ValueError("Duplicate company_ids are not allowed")
+        self.company_ids.append(company_id)
+
+    def insert(self, index, company_id):
+        """
+        Inserts a CompanyId object at the specified index.
+
+        Args:
+            index (int): The index at which to insert the CompanyId object.
+            company_id (CompanyId): The CompanyId object to insert.
+
+        Raises:
+            ValueError: If company_id is not a CompanyId object.
+            ValueError: If company_id is already in the list.
+        """
+        if not isinstance(company_id, CompanyId):
+            raise ValueError("company_id must be an instance of CompanyId")
+        if company_id in self.company_ids:
+            raise ValueError("Duplicate company_ids are not allowed")
+        self.company_ids.insert(index, company_id)
+
+    def remove(self, company_id):
+        """
+        Removes a CompanyId object from the list.
+
+        Args:
+            company_id (CompanyId): The CompanyId object to remove.
+
+        Raises:
+            ValueError: If company_id is not in the list.
+        """
+        if company_id not in self.company_ids:
+            raise ValueError("company_id is not in the list")
+        self.company_ids.remove(company_id)
--- pathfinder_framework/product_footprint/product_id_list.py
+++ pathfinder_framework/product_footprint/product_id_list.py
@@ -2,7 +2,28 @@ from pathfinder_framework.urn import ProductId
 
 
 class ProductIdList:
+    """
+    A list of ProductId objects.
+
+    Attributes:
+        product_ids (list): A list of ProductId objects.
+
+    Raises:
+        ValueError: If product_ids is not a list of ProductId objects.
+        ValueError: If there are duplicate ProductId objects in the list.
+    """
+
     def __init__(self, product_ids):
+        """
+        Initializes a ProductIdList object.
+
+        Args:
+            product_ids (list): A list of ProductId objects.
+
+        Raises:
+            ValueError: If product_ids is not a list of ProductId objects.
+            ValueError: If there are duplicate ProductId objects in the list.
+        """
         if not isinstance(product_ids, list) or not all(isinstance(product_id, ProductId) for product_id in product_ids):
             raise ValueError("product_ids must be a list of ProductId")
         if len(set(product_ids)) != len(product_ids):
@@ -10,23 +31,70 @@ class ProductIdList:
         self.product_ids = product_ids
 
     def __iter__(self):
+        """
+        Returns an iterator over the ProductId objects in the list.
+
+        Returns:
+            iterator: An iterator over the ProductId objects in the list.
+        """
         return iter(self.product_ids)
 
     def __len__(self):
+        """
+        Returns the number of ProductId objects in the list.
+
+        Returns:
+            int: The number of ProductId objects in the list.
+        """
         return len(self.product_ids)
 
     def __getitem__(self, index):
+        """
+        Returns the ProductId object at the specified index.
+
+        Args:
+            index (int): The index of the ProductId object to return.
+
+        Returns:
+            ProductId: The ProductId object at the specified index.
+        """
         return self.product_ids[index]
 
     def __setitem__(self, index, value):
+        """
+        Sets the ProductId object at the specified index.
+
+        Args:
+            index (int): The index of the ProductId object to set.
+            value (ProductId): The ProductId object to set.
+
+        Raises:
+            ValueError: If value is not a ProductId object.
+        """
         if not isinstance(value, ProductId):
             raise ValueError("product_id must be an instance of ProductId")
         self.product_ids[index] = value
 
     def __delitem__(self, index):
+        """
+        Deletes the ProductId object at the specified index.
+
+        Args:
+            index (int): The index of the ProductId object to delete.
+        """
         del self.product_ids[index]
 
     def append(self, product_id):
+        """
+        Appends a ProductId object to the list.
+
+        Args:
+            product_id (ProductId): The ProductId object to append.
+
+        Raises:
+            ValueError: If product_id is not a ProductId object.
+            ValueError: If product_id is already in the list.
+        """
         if not isinstance(product_id, ProductId):
             raise ValueError("product_id must be an instance of ProductId")
         if product_id in self.product_ids:
@@ -34,6 +102,17 @@ class ProductIdList:
         self.product_ids.append(product_id)
 
     def insert(self, index, product_id):
+        """
+        Inserts a ProductId object at the specified index.
+
+        Args:
+            index (int): The index at which to insert the ProductId object.
+            product_id (ProductId): The ProductId object to insert.
+
+        Raises:
+            ValueError: If product_id is not a ProductId object.
+            ValueError: If product_id is already in the list.
+        """
         if not isinstance(product_id, ProductId):
             raise ValueError("product_id must be an instance of ProductId")
         if product_id in self.product_ids:
@@ -41,6 +120,15 @@ class ProductIdList:
         self.product_ids.insert(index, product_id)
 
     def remove(self, product_id):
+        """
+        Removes a ProductId object from the list.
+
+        Args:
+            product_id (ProductId): The ProductId object to remove.
+
+        Raises:
+            ValueError: If product_id is not in the list.
+        """
         if product_id not in self.product_ids:
             raise ValueError("product_id is not in the list")
         self.product_ids.remove(product_id)
