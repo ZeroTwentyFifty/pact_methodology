5df41a8cf3588b07b7a0308e95ec8828f75d3b6b
JohnVonNeumann <louiswillcock@gmail.com>
2024-12-07 14:18:19 +1100

    refactor(product_or_sector_specific_rule_operator.py): expand data model to include str and repr methods"

--- pact_methodology/carbon_footprint/product_or_sector_specific_rule_operator.py
+++ pact_methodology/carbon_footprint/product_or_sector_specific_rule_operator.py
@@ -9,13 +9,16 @@ class ProductOrSectorSpecificRuleOperator(str, Enum):
     - EPD International: for PCRs authored or published by EPD International
     - OTHER: for a PCR not published by the operators mentioned above
 
-    4.13.1. JSON Representation
-    Each value is encoded as a JSON String.
-
     For more information, please refer to the official documentation:
-        https://wbcsd.github.io/tr/2023/data-exchange-protocol-20231207/#dt-productorsectorspecificruleoperator
+        https://wbcsd.github.io/data-exchange-protocol/v2/#dt-productorsectorspecificrule
     """
 
     PEF = "PEF"
     EPD_INTERNATIONAL = "EPD International"
     OTHER = "Other"
+
+    def __str__(self):
+        return self.value
+    
+    def __repr__(self):
+        return f"ProductOrSectorSpecificRuleOperator.{self.name}"
