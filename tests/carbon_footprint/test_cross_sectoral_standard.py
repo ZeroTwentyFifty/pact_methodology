a14b6ab6e546b0bc8b213f6b95550199e6dcfa3c
JohnVonNeumann <louiswillcock@gmail.com>
2024-12-07 14:06:04 +1100

    refactor(cross_sectoral_standard): expand data model to include explicit str and repr methods

--- tests/carbon_footprint/test_cross_sectoral_standard.py
+++ tests/carbon_footprint/test_cross_sectoral_standard.py
@@ -7,3 +7,13 @@ def test_cross_sectoral_standard_values():
     assert CrossSectoralStandard.GHG_PROTOCOL == "GHG Protocol Product standard"
     assert CrossSectoralStandard.ISO_14067 == "ISO Standard 14067"
     assert CrossSectoralStandard.ISO_14044 == "ISO Standard 14044"
+
+def test_cross_sectoral_standard_str():
+    assert str(CrossSectoralStandard.GHG_PROTOCOL) == "GHG Protocol Product standard"
+    assert str(CrossSectoralStandard.ISO_14067) == "ISO Standard 14067"
+    assert str(CrossSectoralStandard.ISO_14044) == "ISO Standard 14044"
+
+def test_cross_sectoral_standard_repr():
+    assert repr(CrossSectoralStandard.GHG_PROTOCOL) == "CrossSectoralStandard.GHG_PROTOCOL"
+    assert repr(CrossSectoralStandard.ISO_14067) == "CrossSectoralStandard.ISO_14067"
+    assert repr(CrossSectoralStandard.ISO_14044) == "CrossSectoralStandard.ISO_14044"
