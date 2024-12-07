15484c4d5a4cc1839ed4d2eea83042e2813c4169
JohnVonNeumann <louiswillcock@gmail.com>
2024-12-07 14:18:19 +1100

    refactor(product_or_sector_specific_rule_operator.py): expand data model to include str and repr methods"

--- tests/carbon_footprint/test_product_or_sector_specific_rule_operator.py
+++ tests/carbon_footprint/test_product_or_sector_specific_rule_operator.py
@@ -7,3 +7,14 @@ def test_product_or_sector_specific_rule_operator_values():
     assert ProductOrSectorSpecificRuleOperator.PEF == "PEF"
     assert ProductOrSectorSpecificRuleOperator.EPD_INTERNATIONAL == "EPD International"
     assert ProductOrSectorSpecificRuleOperator.OTHER == "Other"
+
+
+def test_product_or_sector_specific_rule_operator_str():
+    assert str(ProductOrSectorSpecificRuleOperator.PEF) == "PEF"
+    assert str(ProductOrSectorSpecificRuleOperator.EPD_INTERNATIONAL) == "EPD International"
+    assert str(ProductOrSectorSpecificRuleOperator.OTHER) == "Other"
+
+def test_product_or_sector_specific_rule_operator_repr():
+    assert repr(ProductOrSectorSpecificRuleOperator.PEF) == "ProductOrSectorSpecificRuleOperator.PEF"
+    assert repr(ProductOrSectorSpecificRuleOperator.EPD_INTERNATIONAL) == "ProductOrSectorSpecificRuleOperator.EPD_INTERNATIONAL"
+    assert repr(ProductOrSectorSpecificRuleOperator.OTHER) == "ProductOrSectorSpecificRuleOperator.OTHER"
