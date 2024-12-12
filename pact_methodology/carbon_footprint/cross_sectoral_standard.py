66bf5dc7d8a2d6d291613f3de0c6ffd3fd19c1e4
JohnVonNeumann <louiswillcock@gmail.com>
2024-12-12 21:57:05 +1100

    docs(cross_sectoral_standard.py): update doctest examples

--- pact_methodology/carbon_footprint/cross_sectoral_standard.py
+++ pact_methodology/carbon_footprint/cross_sectoral_standard.py
@@ -1,26 +1,39 @@
 from enum import Enum
 
-
 class CrossSectoralStandard(str, Enum):
     """
-    CrossSectoralStandard is the enumeration of accounting standards used for product carbon footprint calculation. Valid values are
+    CrossSectoralStandard is the enumeration of accounting standards used for product carbon footprint calculation.
+    Valid values are:
 
-    - GHG Protocol Product standard: for the GHG Protocol Product standard
-    - ISO Standard 14067: for ISO Standard 14067
-    - ISO Standard 14044: for ISO Standard 14044
+    - GHG_PROTOCOL: Represents the GHG Protocol Product standard.
+    - ISO_14067: Represents ISO Standard 14067.
+    - ISO_14044: Represents ISO Standard 14044.
 
     Each CrossSectoralStandard MUST be encoded as a JSON string.
 
-    For more information, please refer to the official documentation:
-        https://wbcsd.github.io/data-exchange-protocol/v2/#dt-crosssectoralstandard
+    Examples:
+        Select a standard:
+        >>> standard = CrossSectoralStandard.GHG_PROTOCOL
+        >>> print(standard)
+        GHG Protocol Product standard
+
+        >>> print(repr(standard))
+        CrossSectoralStandard.GHG_PROTOCOL
+
+        Iterating over all standards:
+        >>> for standard in CrossSectoralStandard:
+        ...     print(standard)
+        GHG Protocol Product standard
+        ISO Standard 14067
+        ISO Standard 14044
     """
 
     GHG_PROTOCOL = "GHG Protocol Product standard"
     ISO_14067 = "ISO Standard 14067"
     ISO_14044 = "ISO Standard 14044"
-    
+
     def __str__(self):
         return self.value
-    
+
     def __repr__(self):
         return f"CrossSectoralStandard.{self.name}"
